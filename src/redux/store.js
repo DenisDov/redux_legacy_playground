9c5c35ba38f4974c59bb56b93c7b0e087e94a40c
Denys Dovzhenko <dovzdenis@gmail.com>
2024-05-14 16:34:08 +0300

    un debugger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,7 @@
 import {applyMiddleware, legacy_createStore} from 'redux';
 import {thunk} from 'redux-thunk';
 import reducers from './reducers';
-import {composeWithDevTools} from '@redux-devtools/extension';
 
-const store = legacy_createStore(
-  reducers,
-  composeWithDevTools(applyMiddleware(thunk)),
-);
+const store = legacy_createStore(reducers, applyMiddleware(thunk));
 
 export default store;
